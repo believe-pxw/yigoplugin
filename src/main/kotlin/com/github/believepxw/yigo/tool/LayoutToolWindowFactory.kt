d77c2db86119fd769c8ccd17853604e95e864764
peixw <peixw@bokesoft.com>
2026-01-30 15:57:41 +0800

    隔离窗口宽度

--- src/main/kotlin/com/github/believepxw/yigo/tool/LayoutToolWindowFactory.kt
+++ src/main/kotlin/com/github/believepxw/yigo/tool/LayoutToolWindowFactory.kt
@@ -12,6 +12,9 @@ class LayoutToolWindowFactory : ToolWindowFactory, DumbAware {
         val toolWindowContent = YigoLayoutPanel(project, toolWindow)
         val content = ContentFactory.getInstance().createContent(toolWindowContent, "", false)
         toolWindow.contentManager.addContent(content)
+        
+        // Use SLIDING mode to allow independent width (decoupled from standard Docked windows like Maven)
+        toolWindow.setType(com.intellij.openapi.wm.ToolWindowType.SLIDING, null)
 
         // Focus Mode Logic: Hide other tool windows when this one is active
         project.messageBus.connect().subscribe(com.intellij.openapi.wm.ex.ToolWindowManagerListener.TOPIC, object : com.intellij.openapi.wm.ex.ToolWindowManagerListener {
