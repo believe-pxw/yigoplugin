8df2a40354ef5753898646041ec2d6e6fbb1d9f9
peixw <peixw@bokesoft.com>
2026-02-06 13:57:03 +0800

    增加个矢量图标

---
+++ src/main/kotlin/com/github/believepxw/yigo/icons/YigoIcons.kt
@@ -0,0 +1,9 @@
+package com.github.believepxw.yigo.icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object YigoIcons {
+    @JvmField
+    val Logo: Icon = IconLoader.getIcon("/icons/yigo.svg", YigoIcons::class.java)
+}
